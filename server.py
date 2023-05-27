51c130e7a4a26687b8d76f5c410a98466995ef9c
dk-crazydiv <kartik@deepklarity.com>
2023-05-27 22:36:03 +0530

    Updated code

--- server.py
+++ server.py
@@ -15,7 +15,13 @@ from bark.generation import (
     preload_models,
 )
 from bark.api import semantic_to_waveform
-
+import nltk
+import requests
+import os
+import librosa
+from moviepy.editor import ImageClip, concatenate_videoclips, CompositeVideoClip
+from moviepy.audio.io.AudioFileClip import AudioFileClip
+from moviepy.video import fx
 
 # download and load all models
 preload_models()
@@ -43,6 +49,7 @@ def predict():
 
     return send_file(file_name, mimetype='audio/wav')
 
+
 @app.route('/predict-bulk', methods=['POST']) 
 def predict_bulk():
     content = request.get_json()
@@ -52,22 +59,57 @@ def predict_bulk():
     if "speaker_name" in content:
         history_prompt = content['speaker_name']
 
-    sentences = text.split(".?")
+    file_name = generate_audio_from_text(text, history_prompt=history_prompt)
+    return send_file(file_name, mimetype='audio/wav')
+
+def generate_audio_from_text(text, history_prompt, file_save_path="default"):
+    sentences = nltk.sent_tokenize(text)
     pieces = []
     for sentence in sentences:
-        semantic_tokens = generate_text_semantic(
-            sentence,
-            history_prompt=history_prompt,
-            temp=GEN_TEMP,
-            min_eos_p=0.05,  # this controls how likely the generation is to end
-        )
-
-        audio_array = semantic_to_waveform(semantic_tokens, history_prompt=SPEAKER,)
-        pieces += [audio_array, silence.copy()]
+        audio_array = generate_audio(sentence, history_prompt=history_prompt)
+        pieces += [audio_array]
     audio_array = np.concatenate(pieces)
-    file_name = f"text2flix_{Path(history_prompt).name}.wav"
+    file_name = f"{file_save_path}.wav"
     # save audio to disk
     write_wav(file_name, SAMPLE_RATE, audio_array)
+    return file_name
+
+
+@app.route('/predict-script', methods=['POST']) 
+def predict_script():
+    os.makedirs("images", exist_ok=True)
+    os.makedirs("audio", exist_ok=True)
+    content = request.get_json()
+
+    stitch_data = []
+
+    for idx, scene in enumerate(content["scenes"]):
+        scene_description = scene["scene_description"]
+        image_url = scene["image_url"]
+        image_path = f"images/{idx}.jpg"
+        history_prompt = scene['speaker_name']
+        r = requests.get(image_url, allow_redirects=True)
+        open(image_path, "wb").write(r.content)
+        file_name = generate_audio_from_text(scene_description, history_prompt=history_prompt, file_save_path="audio/"+str(idx))
+        duration = librosa.get_duration(path=file_name)
+        stitch_data.append((image_path, file_name, duration))
+
+    print(stitch_data)
+    # stitch_data = [('images/2.jpg', 'audio/0.wav', 8.186666666666667), ('images/1.jpg', 'audio/1.wav', 10.64)]
+
+    video_clips = []
+    for idx, data in enumerate(stitch_data):
+        image_path, file_name, duration = data
+        image_clip = ImageClip(image_path, duration=duration)  # Adjust the duration as needed
+        audio_clip = AudioFileClip(file_name)
+
+        video_clip = image_clip.set_audio(audio_clip)
+        video_clips.append(video_clip)
+
+    final_clip = concatenate_videoclips(video_clips, method="compose")
+    output_path = "output.mp4"  # Change the file extension as per your needs   
+    final_clip.write_videofile(output_path, codec="libx264", audio_codec="aac",fps=24)
+    return send_file(output_path, mimetype='video/mp4')
 
 if __name__ == '__main__':
    app.run(debug=True) 
